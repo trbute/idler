4e550c9d119c5837979220101fad8af639d80925
tr <tr@trbute.com>
2025-08-02 13:13:28 -0600

    add websocket connection limits

--- src/server/internal/websocket/hub.go
+++ src/server/internal/websocket/hub.go
@@ -1,14 +1,19 @@
 package websocket
 
 import (
+	"log"
+	"time"
+
 	"github.com/google/uuid"
 )
 
 type Hub struct {
-	clients    map[string]*Client // Use tokenID as key instead of userID
-	broadcast  chan *Message
-	register   chan *Client
-	unregister chan *Client
+	clients          map[string]*Client
+	userConnections  map[uuid.UUID][]*ClientInfo
+	broadcast        chan *Message
+	register         chan *Client
+	unregister       chan *Client
+	maxConnections   int
 }
 
 type Message struct {
@@ -18,12 +23,19 @@ type Message struct {
 	Data   map[string]interface{} `json:"data"`
 }
 
+type ClientInfo struct {
+	client    *Client
+	timestamp time.Time
+}
+
 func NewHub() *Hub {
 	return &Hub{
-		clients:    make(map[string]*Client),
-		broadcast:  make(chan *Message, 256),
-		register:   make(chan *Client),
-		unregister: make(chan *Client),
+		clients:         make(map[string]*Client),
+		userConnections: make(map[uuid.UUID][]*ClientInfo),
+		broadcast:       make(chan *Message, 256),
+		register:        make(chan *Client),
+		unregister:      make(chan *Client),
+		maxConnections:  5,
 	}
 }
 
@@ -31,11 +43,19 @@ func (h *Hub) Run() {
 	for {
 		select {
 		case client := <-h.register:
+			h.enforceConnectionLimit(client)
 			h.clients[client.tokenID] = client
+			
+			clientInfo := &ClientInfo{
+				client:    client,
+				timestamp: time.Now(),
+			}
+			h.userConnections[client.userID] = append(h.userConnections[client.userID], clientInfo)
 
 		case client := <-h.unregister:
 			if _, ok := h.clients[client.tokenID]; ok {
 				delete(h.clients, client.tokenID)
+				h.removeUserConnection(client)
 				close(client.send)
 			}
 
@@ -102,7 +122,6 @@ func (h *Hub) SendSystemMessage(message string) {
 
 func (h *Hub) DisconnectClientByToken(tokenID string) {
 	if client, ok := h.clients[tokenID]; ok {
-		// Send session expired message
 		select {
 		case client.send <- &Message{
 			Type: "error",
@@ -111,7 +130,51 @@ func (h *Hub) DisconnectClientByToken(tokenID string) {
 		default:
 		}
 		
-		// Trigger unregister
 		h.unregister <- client
+	}
+}
+
+func (h *Hub) enforceConnectionLimit(newClient *Client) {
+	userConnections := h.userConnections[newClient.userID]
+	
+	if len(userConnections) >= h.maxConnections {
+		oldestIndex := 0
+		oldestTime := userConnections[0].timestamp
+		
+		for i, conn := range userConnections {
+			if conn.timestamp.Before(oldestTime) {
+				oldestIndex = i
+				oldestTime = conn.timestamp
+			}
+		}
+		
+		oldestClient := userConnections[oldestIndex].client
+		log.Printf("User %s exceeded connection limit (%d), disconnecting oldest connection", 
+			newClient.userID.String(), h.maxConnections)
+		
+		select {
+		case oldestClient.send <- &Message{
+			Type: "error",
+			Data: map[string]interface{}{"message": "Connection limit exceeded. Disconnecting oldest session."},
+		}:
+		default:
+		}
+		
+		h.unregister <- oldestClient
+	}
+}
+
+func (h *Hub) removeUserConnection(client *Client) {
+	userConnections := h.userConnections[client.userID]
+	
+	for i, conn := range userConnections {
+		if conn.client == client {
+			h.userConnections[client.userID] = append(userConnections[:i], userConnections[i+1:]...)
+			break
+		}
+	}
+	
+	if len(h.userConnections[client.userID]) == 0 {
+		delete(h.userConnections, client.userID)
 	}
 }
